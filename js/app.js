04f84567bedeca680bc2b393ce532afc428cc200
Victor Lascares <vmlascares@gmail.com>
2022-09-10 17:05:32 -0500

    Cargando criptomonedas al select

--- js/app.js
+++ js/app.js
@@ -17,6 +17,18 @@ function consultarCriptomonedas() {
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => obtenerCriptomonedas(resultado.Data))
-        .then(criptomonedas => console.log(criptomonedas))
+        .then(criptomonedas => selectCriptomonedas(criptomonedas))
         .catch(error => console.log(error));
+}
+
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach(cripto => {
+        const { FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedaSelect.appendChild(option);
+    })
 }
